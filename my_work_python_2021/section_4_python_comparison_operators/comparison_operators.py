acc450373754b4571622abf9e773ef6a00b280c0
euanstirling <euanstirling@gmail.com>
2021-01-20 10:43:35 +0000

    section 4 - comparison operators

---
+++ my_work_python_2021/section_4_python_comparison_operators/comparison_operators.py
@@ -0,0 +1,23 @@
+
+#! COMPARISON OPERATORS
+
+# * Remember the following points
+# * Equality ==
+# = is for assigning a variable not equals
+# Capitalisation with strings does matter
+# Also consider types (int 2 does not equal string "two")
+
+# * Inequality != > <
+# same rules regarding equality
+
+# * Chain Comparison operators (logical operators)
+# and, or, not
+
+# eg
+1 < 2 and 2 < 3
+("h" == "h") and (3 == 3)  # TRUE
+
+1 == 2 or 2 == 3  # FALSE
+
+# "not" returns the opposite boolean
+not 1 == 1  # statement is true but "not" makes it return false
