a6b91ebc32131def9732a358062ac831d01c2ce9
euanstirling <euanstirling@gmail.com>
2021-01-22 17:09:14 +0000

    solutions

--- my_work_python_2021/section_5_python_statements/python_statements_tests_and_solutions.py
+++ my_work_python_2021/section_5_python_statements/python_statements_tests_and_solutions.py
@@ -18,6 +18,11 @@ for num in x:
 
 print(list(range(0, 11, 2)))
 
+# or
+
+for num in range(0, 11, 2):
+    print(num)
+
 # * 3. Use a List Comprehension to create a list of all numbers between 1 and 50 that are divisible by 3.
 
 list = [x for x in range(1, 51) if x % 3 == 0]
@@ -32,7 +37,29 @@ print(numbers)
 # * 4. Go through the string below and if the length of a word is even print "even!"
 # * st = 'Print every word in this sentence that has an even number of letters'
 
+st2 = 'Print every word in this sentence that has an even number of letters'
+
+for word in st2.split():
+    if len(word) % 2 == 0:
+        print(word + " is even")
+
+
 # * 5. Write a program that prints the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number, and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
 
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("fizzbuzz")
+    elif num % 3 == 0:
+        print("fizz")
+    elif num % 5 == 0:
+        print("buzz")
+    else:
+        print(num)
+
 # * 6. Use List Comprehension to create a list of the first letters of every word in the string below:
-# * st = 'Create a list of the first letters of every word in this string'
+# * st3 = 'Create a list of the first letters of every word in this string'
+
+
+st3 = 'Create a list of the first letters of every word in this string'
+
+print([word[0] for word in st3.split()])
